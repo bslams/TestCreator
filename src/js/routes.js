3ed1d1c9630c227c73a979c3c1f6a1374e97104b
bslams <bslams@hotmail.com>
2018-11-26 13:57:14 +0700

    init project

---
+++ src/js/routes.js
@@ -0,0 +1,39 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('page5', {
+    url: '/page5',
+    templateUrl: 'templates/page5.html',
+    controller: 'page5Ctrl'
+  })
+
+  .state('page6', {
+    url: '/page6',
+    templateUrl: 'templates/page6.html',
+    controller: 'page6Ctrl'
+  })
+
+  .state('page7', {
+    url: '/page7',
+    templateUrl: 'templates/page7.html',
+    controller: 'page7Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+
+});
